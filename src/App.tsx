f21f5cac1750bee34d1fb433324d360be100c29b
jendhordejan <jendhordejan@gmail.com>
2021-04-15 22:39:50 +0200

    first commit: -initialize project -setup basic Global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { GlobalStyle } from './global/GlobalStyle'
 
 function App() {
   return (
+    <>
+    <GlobalStyle />
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>All rooms at Hotel The Craftsmen vary in size, facilities, design, antiques and Dutch historical crafts. Let's see which room is perfect for you! All room rates include breakfast.</p>
     </div>
+    </>
   );
 }
 
---
+++ src/global/GlobalStyle.tsx
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+      box-sizing: border-box;
+    }
+
+    html,
+    body {
+      font-family: 'Antic Didone', serif;
+      color: #2b3a44;
+
+    body {
+        background: linear-gradient( to left, #ffffff 50%, rgba(230, 237, 238, 0.377) 50%);
+        opacity: 0.8;
+    }
+    }
+`
